d02aa95a57cfe098e354f35b11f6a1c54094baee
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-08-11 12:59:04 +0800

    fix code

--- main.py
+++ main.py
@@ -9,31 +9,42 @@ chat_id = os.environ['CHAT_ID']
 api_key = os.environ['TELEGRAM_API_KEY']
 news_api_key = os.environ['NEWS_API']
 
-# create html content for telegraph page
-newsapi = NewsApiClient(api_key=news_api_key)
-top_headlines = newsapi.get_top_headlines(language='en', country='in')
 
-if not top_headlines:
-    print("No articles found")
-    exit()
+def send_news(language: str, country: str):
+    # create html content for telegraph page
+    newsapi = NewsApiClient(api_key=news_api_key)
+    top_headlines = newsapi.get_top_headlines(language=language, country=country)
 
-page_content = ''
-for article in top_headlines['articles']:
-    title = f'<h3>{article["title"]}</h3>\n'
-    date = f'<blockquote>{article["publishedAt"].replace("T", " ").replace("Z", " UTC")}</blockquote>\n'
-    description = f'<p><b>{article["description"]}</b></p>\n'
-    img = f'<img src=\"{article["urlToImage"]}\"></img>\n'
-    content = f'<p>{article["content"].split("[")[0].replace("<", "").replace(">", "") if article["content"] != None else ""}<a href=\"{article["url"]}\">click here</a></p>\n'
-    page_content += title + date + description + img + content + '<hr></hr>\n'
+    if not top_headlines:
+        print("No articles found")
+        exit()
 
-# create a telegraph page
-tg = Telegraph(auth_token)
-response = tg.create_page('News Telegraph Today', html_content=page_content)
+    page_content = ''
+    for article in top_headlines['articles']:
+        title = f'<h3>{article["title"]}</h3>\n'
+        date = f'<blockquote>{article["publishedAt"].replace("T", " ").replace("Z", " UTC")}</blockquote>\n'
+        description = f'<p><b>{article["description"]}</b></p>\n'
+        img = f'<img src=\"{article["urlToImage"]}\"></img>\n'
+        content = f'<p>{article["content"].split("[")[0].replace("<", "").replace(">", "") if article["content"] != None else ""}<a href=\"{article["url"]}\">click here</a></p>\n'
+        page_content += title + date + description + img + content + '<hr></hr>\n'
 
-# send it via telegram
-bot = telebot.TeleBot(api_key)
-bot.send_message(chat_id, response['url'])
+    # create a telegraph page
+    tg = Telegraph(auth_token)
+    response = tg.create_page('News Telegraph Today', html_content=page_content)
 
-# log the newspapers
-with open('newslogs.txt', 'a') as file:
-    file.writelines(f'{str(datetime.now())} - {response["url"]}\n')
+    # send it via telegram
+    bot = telebot.TeleBot(api_key)
+    bot.send_message(chat_id, response['url'])
+
+    # log the newspapers
+    with open('newslogs.txt', 'a') as file:
+        file.writelines(f'{str(datetime.now())} - {response["url"]}\n')
+
+
+def main():
+    send_news('en', 'us')
+    send_news('zh', 'cn')
+
+
+if __name__ == '__main__':
+    main()
