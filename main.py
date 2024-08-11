a20431799f3ea3e648a86036fc86a6be8870d7e5
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-08-11 12:43:34 +0800

    fix code

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ news_api_key = os.environ['NEWS_API']
 
 # create html content for telegraph page
 newsapi = NewsApiClient(api_key=news_api_key)
-top_headlines = newsapi.get_top_headlines(language='en',country='in')
+top_headlines = newsapi.get_top_headlines(language='en', country='in')
 
 if not top_headlines:
     print("No articles found")
@@ -20,20 +20,20 @@ if not top_headlines:
 page_content = ''
 for article in top_headlines['articles']:
     title = f'<h3>{article["title"]}</h3>\n'
-    date = f'<blockquote>{article["publishedAt"].replace("T"," ").replace("Z"," UTC")}</blockquote>\n'
+    date = f'<blockquote>{article["publishedAt"].replace("T", " ").replace("Z", " UTC")}</blockquote>\n'
     description = f'<p><b>{article["description"]}</b></p>\n'
     img = f'<img src=\"{article["urlToImage"]}\"></img>\n'
-    content = f'<p>{article["content"].split("[")[0].replace("<","").replace(">","") if article["content"] != None else ""}<a href=\"{article["url"]}\">click here</a></p>\n'
-    page_content += title+date+description+img+content+'<hr></hr>\n'
+    content = f'<p>{article["content"].split("[")[0].replace("<", "").replace(">", "") if article["content"] != None else ""}<a href=\"{article["url"]}\">click here</a></p>\n'
+    page_content += title + date + description + img + content + '<hr></hr>\n'
 
 # create a telegraph page
 tg = Telegraph(auth_token)
-response = tg.create_page('Telegraph Today',html_content=page_content)
+response = tg.create_page('News Telegraph Today', html_content=page_content)
 
 # send it via telegram
 bot = telebot.TeleBot(api_key)
-bot.send_message(chat_id,response['url'])
+bot.send_message(chat_id, response['url'])
 
-#log the newspapers
-with open('newslogs.txt','a') as file:
+# log the newspapers
+with open('newslogs.txt', 'a') as file:
     file.writelines(f'{str(datetime.now())} - {response["url"]}\n')
